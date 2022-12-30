4db14196e9ab806243eed1a66596e7809c26aec5
jennifershih <s01059810@gmail.com>
2022-12-30 15:09:54 +0800

    feat: sign in with google

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  SocialAuthService,
+  SocialUser,
+  GoogleLoginProvider,
+} from '@abacritt/angularx-social-login';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'money-tracker';
+export class AppComponent implements OnInit {
+  title = 'moneyTracker';
+
+  user: SocialUser | undefined;
+
+  private accessToken: string | undefined;
+
+  get loggedIn() {
+    return this.user != null;
+  }
+
+  constructor(private authService: SocialAuthService) {}
+
+  ngOnInit() {
+    this.authService.authState.subscribe((user) => {
+      this.user = user;
+    });
+  }
+
+  async getAccessToken() {
+    this.accessToken = await this.authService.getAccessToken(
+      GoogleLoginProvider.PROVIDER_ID
+    );
+    return this.accessToken;
+  }
+
+  async refreshAuthToken() {
+    await this.authService.refreshAuthToken(GoogleLoginProvider.PROVIDER_ID);
+  }
+
+  async refreshAccessToken() {
+    await this.authService.refreshAccessToken(GoogleLoginProvider.PROVIDER_ID);
+  }
+
+  async signOut() {
+    await this.authService.signOut();
+  }
 }
